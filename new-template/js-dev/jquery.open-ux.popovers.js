a0ebe490efc521bca20e9e23af9f41c0342d4720
Jeff Herb <herb.jeff@gamil.com>
2012-04-27 14:31:33 -0400

    new icon test area. Added popovers to new template

--- new-template/js-dev/jquery.open-ux.popovers.js
+++ new-template/js-dev/jquery.open-ux.popovers.js
@@ -4,31 +4,88 @@
 	// Attach the new plugin onto the jQuery object.
 	$.fn.extend({
 
-		popover: function() {
+		popover: function(options) {
+
+			var defaults = {
+				arrow: false
+            };
+
+			// Check to see if browser supports rotating css
+			if ($('html').is('.csstransforms')) {
+				defaults.arrow = true;
+			}
+
+            var options =  $.extend(defaults, options);
 
 			return this.each(function() {
-				
-				// Save off the current popover container info
-				var container = $(this);
 
-				// Save of the binding link... Should be declared right above the popover container
-				var link = container.prev('a');
+				// Get plugin defaults
+				var opt = options;
 
-				// Setup the click event that runs every time a popover is clicked
-				link.on("click", function(e){
+				// Save off the popover container
+				var obj = $(this);
 
-					// Prevent the original click action
+				// See if we need to add arrow
+				if(opt.arrow) {
+					obj.prepend($('<div>').addClass('arrow'));
+					var arrow = obj.children('.arrow');
+				}
+
+				var link = obj.prev('a');
+
+				link.on('click', function(e) {
+
+					// Stop the anchor from moving forward.
 					e.preventDefault();
-					
-					// Get the links current location
+
+					// Get the anchors position
 					var linkPos = link.offset(), top, left;
 
+					// Popover location
 					top = linkPos.top + link.height();
-					left = linkPos.left - (container.outerWidth()/2) + (link.width()/2);
+					left = linkPos.left - (obj.outerWidth()/2) + (link.width()/2);
 
-					container.css({top:top,left:left}).show();
+					// Check to make sure if the the left if off the screen if so balance it.
+					if (left <= 0) {
 
-					//alert(linkPos);
+						// Check to see if we also need to reposition the window
+						if(opt.arrow) {
+
+							// 5 reflects half the width of the arrow.
+							//var aLeft = (left * -1) + 5;
+							var aLeft = obj.outerWidth()/2 - link.width()/2
+
+							// Apply new styles
+							arrow.css({left:aLeft});
+						}
+
+						// Set left to be 10px away from the side of the window.
+						left = 10; 
+
+					}
+
+					// Check to see if the popover flows over the right side of the screen
+					if ((left+obj.outerWidth()) > $(window).width()){
+
+						// Check to see if we also need to reposition the window
+						if(opt.arrow) {
+
+							// 5 reflects half the width of the arrow.
+							var aLeft = link.width()/2 + obj.outerWidth()/2;
+
+							// Apply new styles
+							arrow.css({left:aLeft});
+						}
+						
+						// Set left to be 10px away from the side of the window.
+						left = $(window).width() - obj.outerWidth() - 10;
+
+					}
+
+					// Set location onot the popover container
+					obj.css({top:top,left:left});
+
+					obj.show();
 
 				});
 
