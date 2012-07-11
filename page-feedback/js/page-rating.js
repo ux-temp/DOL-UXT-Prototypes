754a351a9a222175ec8a229e399e946ca2bf6d2e
Jeff Herb <herb.jeff@gmail.com>
2012-07-11 15:21:45 -0400

    added interaction to page feedback

---
+++ page-feedback/js/page-rating.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+
+	function removeOtherRating(rating, upDown){
+		var listParent = rating.parent();
+
+		// Add undo click to selected rating int the variable to be read
+		var undo = $('<a></a>').attr('href','#').addClass('change-rating').text('Change').on('click', function(e){
+			e.preventDefault();
+			e.stopPropagation();
+
+			if (upDown) {
+
+				restorOtherRating(upDown);
+			} else {
+				restorOtherRating(upDown);
+			}
+
+		});	
+
+		listParent.addClass('vote').siblings().slideUp().animate({ opacity: 0 }, {duration: 500, queue: false});
+		undo.appendTo(rating);
+
+		if (upDown) {
+
+			$('#written-feedback').slideDown().animate({ opacity: 1 }, {duration: 500, queue: false});
+
+		}
+
+
+
+	}
+
+	function restorOtherRating(written){
+		var changeLinks = $('.change-rating');
+		var currentSelection = changeLinks.parents('li.vote');
+
+		currentSelection.removeClass('vote').siblings().slideDown().animate({ opacity: 1 }, {duration: 500, queue: false});
+
+		if (written) {
+			$('#written-feedback').slideUp().animate({ opacity: 0 }, {duration: 500, queue: false});
+		}
+
+
+		changeLinks.remove();
+
+	}
+
+
+	var upvote = $('#ux-page-rating .upvote');
+	var downvote = $('#ux-page-rating .downvote');
+
+	upvote.on('click', function(e){
+
+		e.preventDefault();
+
+		removeOtherRating(upvote, false);
+
+	});
+
+	downvote.on('click', function(e){
+
+		e.preventDefault();
+
+		removeOtherRating(downvote, true);
+
+	});
+
+});
