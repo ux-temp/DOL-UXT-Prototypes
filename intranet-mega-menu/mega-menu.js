40cc407b0d557ef6949680f9e576f97b7eb95ad3
PaulPulcher <paulpulcher@gmail.com>
2012-08-10 08:54:19 -0400

    Update

--- intranet-mega-menu/mega-menu.js
+++ intranet-mega-menu/mega-menu.js
@@ -1,59 +1,177 @@
 var control_loop = 0;
 var mega_menu = {
-    "menu-test": {
+    "cat-home":{
+    },
+    "cat-5": {
         "size": 17,
         "content": {
             "container_size-1": "col_4",
             "container_contents-1": {
-				"header-1": "Job Seekers",
+				"header-1": "Administrative Finance Bureau",
+                "header-2": "Communications",
+                "header-3": "Division of Employment Workforce Solutions"
+            },
+            "mega-menu-landing": {
+                "url": "More Resources",
+                "link": "#"
+            },
+            "container_size-2": "col_4",
+            "container_contents-2": {
+                "header-1": "Emergency Management",
+                "header-2": "Equal Opportunity Development",
+                "header-3": "Human Resources",
+                "header-4": "Labor Services"
+            },
+            "mega-menu-landing": {
+                "url": "More Resources",
+                "link": "#"
+            },
+            "container_size-3": "col_4",
+            "container_contents-3": {
+                "header-1": "Office of Staff and Organizational Development",
+                "header-2": "Planning and Technology",
+                "header-3": "Public Work"
+            },
+            "mega-menu-landing": {
+                "url": "More Resources",
+                "link": "#"
+            },
+            "container_size-3": "col_4",
+            "container_contents-3": {
+                "header-1": "Research & Statistics",
+                "header-2": "Safety & Health",
+                "header-3": "Special Investigations"
+            },
+            "mega-menu-landing": {
+                "url": "More Resources",
+                "link": "#"
+            },
+            "container_size-4": "col_4",
+            "container_contents-4": {
+                "header-1": "Unemployment Appeal Board",
+                "header-2": "Unemployment Benefits",
+                "header-3": "Unemplyment Insurance",
+                "header-4": "Worker Protection"
+            },
+            "mega-menu-landing": {
+                "url": "More Resources",
+                "link": "#"
+            }
+        }
+    },
+
+
+    "cat-2": {
+        "size": 17,
+        "content": {
+            "container_size-1": "col_4",
+            "container_contents-1": {
+                "header-1": "Job Seekers",
                 "link-1": {
-                    "url": "http://labor.ny.gov/careerservices/findajob/jobfairrecruitmentsindex.shtm",
+                    "url": "#",
                     "text": "Career Fairs and Recruitments"
                 },
                 "link-2": {
-                    "url": "http://labor.ny.gov/careerservices/CareerServicesIndex.shtm",
+                    "url": "http://www.example.com",
                     "text": "Find a Job"
-                },
-                "link-3": {
-                    "url": "http://labor.ny.gov/workforcenypartners/osview.asp",
-                    "text": "One-Stop Career Centers"
-                },
-                "header-2": "Unemployment",
-                "link-4": {
-                    "url": "http://labor.ny.gov/unemploymentassistance.shtm",
-                    "text": "Unemployment Assistance"
-                },
-                "link-5": {
-                    "url": "http://labor.ny.gov/ui/claimantinfo/UIBenefitsCalculator.shtm",
-                    "text": "Benefits Calculator"
-                },
-                "link-6": {
-                    "url": "http://labor.ny.gov/ui/claimantinfo/ExtendedBenefits.shtm",
-                    "text": "Extended Benefits"
-                },
-                "link-7": {
-                    "url": "https://ui.labor.state.ny.us/UBC/home.do",
-                    "text": "Claim Weekly Benefits"
-                },
-                "link-8": {
-                    "url": "http://labor.ny.gov/agencyinfo/report-fraud.shtm",
-                    "text": "Report Fraud"
-                },
-				"link-9": {
-                    "url": "http://labor.ny.gov/formsdocs/ui/claimant-forms-and-publications.shtm",
-                    "text": "Forms and Publications"
-                },
-                "linkheader-3": {
-                    "url": "http://labor.ny.gov/vets/vetintropage.shtm",
-                    "text": "Veterans' Services"
                 }
             },
             "mega-menu-landing": {
                 "url": "More Resources",
-                "link": "http://labor.ny.gov/main/individuals.asp"
+                "link": "#"
+            }
+        }
+    },
+
+    "cat-3": {
+        "size": 17,
+        "content": {
+            "container_size-1": "col_4",
+            "container_contents-1": {
+                "header-1": "Job Seekers",
+                "link-1": {
+                    "url": "#",
+                    "text": "Career Fairs and Recruitments"
+                },
+                "link-2": {
+                    "url": "http://www.example.com",
+                    "text": "Find a Job"
+                }
+            },
+            "mega-menu-landing": {
+                "url": "More Resources",
+                "link": "#"
+            }
+        }
+    },
+
+    "cat-4": {
+        "size": 17,
+        "content": {
+            "container_size-1": "col_4",
+            "container_contents-1": {
+                "header-1": "Job Seekers",
+                "link-1": {
+                    "url": "#",
+                    "text": "Career Fairs and Recruitments"
+                },
+                "link-2": {
+                    "url": "http://www.example.com",
+                    "text": "Find a Job"
+                }
+            },
+            "mega-menu-landing": {
+                "url": "More Resources",
+                "link": "#"
+            }
+        }
+    },
+
+    "cat-1": {
+        "size": 17,
+        "content": {
+            "container_size-1": "col_4",
+            "container_contents-1": {
+                "header-1": "Job Seekers",
+                "link-1": {
+                    "url": "#",
+                    "text": "Career Fairs and Recruitments"
+                },
+                "link-2": {
+                    "url": "http://www.example.com",
+                    "text": "Find a Job"
+                }
+            },
+            "mega-menu-landing": {
+                "url": "More Resources",
+                "link": "#"
+            }
+        }
+    },
+
+    "cat-6": {
+        "size": 17,
+        "content": {
+            "container_size-1": "col_4",
+            "container_contents-1": {
+                "header-1": "Job Seekers",
+                "link-1": {
+                    "url": "#",
+                    "text": "Career Fairs and Recruitments"
+                },
+                "link-2": {
+                    "url": "http://www.example.com",
+                    "text": "Find a Job"
+                }
+            },
+            "mega-menu-landing": {
+                "url": "More Resources",
+                "link": "#"
             }
         }
     }
+
+
 }
 
 /*
