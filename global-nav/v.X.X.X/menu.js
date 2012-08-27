7be35cdc0bd1934650acae9e0d7a046bd74b0fd7
Jeff Herb <herb.jeff@gmail.com>
2012-08-27 18:18:21 -0400

    new mega-menu

---
+++ global-nav/v.X.X.X/menu.js
@@ -0,0 +1,433 @@
+var control_loop = 0;
+var mega_menu = {
+	"unemployment":
+		{
+			"size":4,
+			"content": {
+				"container_size-1":"col_2",
+				"container_contents-1": {
+					"linkheader-1": {
+						"url":"http://www.labor.ny.gov/ui/claimantinfo/UIBenefitsCalculator.shtm",
+						"text":"Benefits Calculator"
+					},
+					"linkheader":{
+						"url":"http://www.labor.ny.gov/ui/ui_index.shtm",
+						"text":"Claimants Information"
+					},
+					"link-1": {
+						"url":"http://www.labor.ny.gov/ui/claimantinfo/ExtendedBenefits.shtm",
+						"text":"Benefit Payment Inquiry"
+					},
+					"link-2": {
+						"url":"http://www.labor.ny.gov/ui/claimantinfo/Claimant%20Handbook%20-%20Languages.shtm",
+						"text":"Claimant Handbook"
+					},
+					"link-3": {
+						"url":"http://www.labor.ny.gov/ui/claimantinfo/ExtendedBenefits.shtm",
+						"text":"Extended Benefits"
+					},
+					"link-4": {
+						"url":"http://www.labor.ny.gov/ui/claimantinfo/hearingfaq.shtm",
+						"text":"Hearings and Appeals"
+					},
+					"link-5": {
+						"url":"http://www.labor.ny.gov/ui/claimantinfo/WarningsandAdvisoriespage.shtm",
+						"text":"UI Warnings & Advisories"
+					},
+					"linkheader-2": {
+						"url":"https://ui.labor.state.ny.us/UBC/home.do",
+						"text":"File a Claim"
+					}
+				},
+				"container_size-2":"col_2",
+				"container_contents-2": {
+					"linkheader-1":{
+						"url":"http://www.labor.ny.gov/ui/ui_index.shtm",
+						"text":"Employer Information"
+					},
+					"link-1": {
+						"url":"http://www.labor.ny.gov/ui/dande/covered1.shtm",
+						"text":"Covered Employment"
+					},
+					"link-2": {
+						"url":"http://www.labor.ny.gov/ui/claimantinfo/ExtendedBenefits.shtm",
+						"text":"Extended Benefits"
+					},
+					"link-3": {
+						"url":"http://www.labor.ny.gov/ui/bpta/UI%20IMPORTANT%20NOTICE%20TO%20EMPLOYERS%20IAS.shtm",
+						"text":"Information on Interest Assessment Surcharge"
+					},
+					"link-4": {
+						"url":"http://www.labor.ny.gov/ui/employerinfo/cobraforemployers.shtm",
+						"text":"Information regarding the COBRA subsidy"
+					},
+					"link-5": {
+						"url":"http://www.labor.ny.gov/ui/MisclassificationofWorkers.shtm",
+						"text":"Misclassification of Workers"
+					},
+					"linkheader-2": {
+						"url":"http://www.labor.ny.gov/unemployment-guidance-for-state-government-employees.shtm",
+						"text":"Unemployment Guidance for Government Employees"
+					}
+				}
+			}
+		},	
+	"jobseekers":
+		{
+			"size":4,
+			"adjust": {
+				"over":"200"
+			},
+			"content":{
+				"container_size-1":"col_2",
+				"container_contents-1": {
+					"header-1":"Job Searching Tools",
+					"link-1": {
+						"url":"http://www.labor.ny.gov/apprenticeship/Appindex.shtm",
+						"text":"Apprenticeship Opportunities"
+					},
+					"link-2": {
+						"url":"http://careerzone.ny.gov/",
+						"text":"CareerZone"
+					},
+					"link-3": {
+						"url":"http://www.labor.ny.gov/stats/cslist.shtm",
+						"text":"Civil Service Exams"
+					},
+					"link-4": {
+						"url":"http://www.labor.ny.gov/careerservices/findajob/JobFairRecruitmentsIndex.shtm",
+						"text":"Fairs and Recruitments Events"
+					},
+					"link-5": {
+						"url":"http://www.jobcentral.org/ny/",
+						"text":"Job Central"
+					},
+					"link-6": {
+						"url":"http://www.labor.ny.gov/careerservices/findajob/tableco.shtm",
+						"text":"Job Searching Guide"
+					},
+					"link-7": {
+						"url":"https://www.nycareerzone.org/jobseeker/login.do?url=%2Fjobseeker%2Fportfolio%2Findex.jsp",
+						"text":"JobZone"
+					}
+				},
+				"container_size-2":"col_2",
+				"container_contents-2": {
+					"linkheader-1":{
+						"url":"http://www.labor.ny.gov/workforcenypartners/osview.asp",
+						"text":"One-Stop Career Centers"
+					},
+					"linkheader-2":{
+						"url":"http://www.labor.ny.gov/VETs/VETINTROPAGE.shtm",
+						"text":"Veterans Come First!"
+					},
+					"link-1": {
+						"url":"http://www.labor.ny.gov/VETs/EmploySpec/VeteransEmploySpec.shtm",
+						"text":"Career Counseling"
+					},
+					"link-2": {
+						"url":"http://www.cs.state.ny.us/dpm/wartime_vets.cfm",
+						"text":"Information on Civil Service Positions"
+					},
+					"link-3": {
+						"url":"http://www.veterans.state.ny.us/",
+						"text":"Veterans Affairs"
+					}
+				}
+			}
+		},	
+	"businesses":
+		{
+			"size":4,
+			"adjust": {
+				"over":"100"
+			},
+			"content": {
+				"container_size-1":"col_2",
+				"container_contents-1": {
+					"linkheader-1":{
+						"url":"http://labor.ny.gov/businessservices/BusinessServicesIndex.shtm",
+						"text":"Business Services"
+					},
+					"link-1": {
+						"url":"http://labor.ny.gov/businessservices/localcontacts/LocalContactsIndex.shtm",
+						"text":"Local Contacts"
+					},
+					"link-2": {
+						"url":"http://labor.ny.gov/businessservices/trainworkforce/TrainingYourWorkforceIndex.shtm",
+						"text":"Training your Workforce"
+					},
+					"link-3": {
+						"url":"http://labor.ny.gov/businessservices/services/ServicesIndex.shtm",
+						"text":"Other Services"
+					},						
+					"linkheader-2":{
+						"url":"https://dbr.labor.state.ny.us/EDList/searchPage.do",
+						"text":"Debarred Employers"
+					},					
+					"linkheader-3":{
+						"url":"http://www.labor.ny.gov/agencyinfo/moa/moa.shtm",
+						"text":"Method of Adiministration"
+					},
+					"linkheader-4":{
+						"url":"http://www.labor.ny.gov/ui/dande/register1.shtm",
+						"text":"Register As A Employer"
+					},
+					"linkheader-5":{
+						"url":"http://labor.ny.gov/ui/dande/sharedwork1.shtm",
+						"text":"Shared Work Program"
+					}
+				},
+				"container_size-2":"col_2",
+				"container_contents-2": {
+					"linkheader-1":{
+						"url":"http://www.labor.ny.gov/ui/bpta/TaxTable.shtm",
+						"text":"UI Tax Table"
+					},
+					"linkheader-2":{
+						"url":"http://www.labor.ny.gov/workforcenypartners/wia.shtm",
+						"text":"Workforce Investment Act"
+					},
+					"link-1": {
+						"url":"http://applications.labor.ny.gov/ETPL/",
+						"text":"Eligible Training Provider List"
+					},
+					"link-2": {
+						"url":"http://www.labor.ny.gov/workforcenypartners/lwia.shtm",
+						"text":"Local Workforce Investment Areas Information"
+					},
+					"link-3": {
+						"url":"http://www.labor.ny.gov/workforcenypartners/rr.shtm",
+						"text":"Rapid Response and Business Retention"
+					},	
+					"link-4": {
+						"url":"http://www.labor.ny.gov/workforcenypartners/swibs.shtm",
+						"text":"State Workforce Investment"
+					},	
+					"linkheader-3":{
+						"url":"http://www.labor.ny.gov/businessservices/EDSU/EDSU%20WOTC.shtm",
+						"text":"Work Opportunity Tax Credit"
+					}
+				}
+			}
+		},	
+	"workers" :
+		{
+			"size":5,
+			"adjust": {
+				"over":"350"
+			},
+			"content": {
+				"container_size-1":"col_2",
+				"container_contents-1": {
+					"linkheader-1": {
+						"url":"http://www.labor.ny.gov/BIWR/index.shtm",
+						"text":"Bureau of Immigrant Workers Rights"
+					},
+					"linkheader-2": {
+						"url":"http://www.labor.ny.gov/workerprotection/laborstandards/labor_standards.shtm",
+						"text":"Labor Standards"
+					},
+					"link-1": {
+						"url":"http://www.labor.ny.gov/workerprotection/laborstandards/workprot/garment.asp",
+						"text":"Apparel Industry Task Force"
+					},
+					"link-2": {
+						"url":"http://www.labor.ny.gov/workerprotection/laborstandards/workprot/minors.shtm",
+						"text":"Employment of Minors"
+					},					
+					"link-3": {
+						"url":"http://www.labor.ny.gov/workerprotection/laborstandards/employer/peo.shtm",
+						"text":"Professional Employer Associations"
+					},
+					"link-4": {
+						"url":"http://www.labor.ny.gov/workerprotection/laborstandards/workprot/wphmpg.shtm",
+						"text":"Working Papers"
+					}
+				},
+				"container_size-2":"col_2",
+				"container_contents-2": {
+					"linkheader-1": {
+						"url":"http://www.labor.ny.gov/workerprotection/safetyhealth/DOSH_INDEX.shtm",
+						"text":"Safety and Health"
+					},
+					"link-1": {
+						"url":"http://www.labor.ny.gov/workerprotection/safetyhealth/DOSH_ASBESTOS.shtm",
+						"text":"Asbestos Control Bureau"
+					},					
+					"link-2": {
+						"url":"http://www.labor.ny.gov/workerprotection/safetyhealth/DOSH_CODE_RULES.shtm",
+						"text":"Codes and Rules"
+					},
+					"link-3": {
+						"url":"http://www.labor.ny.gov/workerprotection/safetyhealth/DOSH_ONSITE_CONSULTATION.shtm",
+						"text":"Forms and Documentation"
+					},
+					"link-4": {
+						"url":"http://www.labor.ny.gov/workerprotection/safetyhealth/DOSH_ONSITE_CONSULTATION.shtm",
+						"text":"Onsite Consultation"
+					}
+				},
+				"container_size-3":"col_1",
+				"container_contents-3": {
+					"linkheader-1": {
+						"url":"http://www.labor.ny.gov/workerprotection/laborstandards/workprot/lshmpg.shtm",
+						"text":"Wages and Hours"
+					},
+					"link-1": {
+						"url":"http://www.labor.ny.gov/workerprotection/laborstandards/workprot/minwage.shtm",
+						"text":"Minimum Wages"
+					},
+					"link-2": {
+						"url":"http://wpp.labor.state.ny.us/wpp/publicViewPWChanges.do?method=showIt",
+						"text":"Prevaling Wage Schedule"
+					},
+					"linkheader-2": {
+						"url":"http://www.labor.ny.gov/workforcenypartners/wfnyp_index.shtm",
+						"text":"Workforce New York"
+					},					
+					"linkheader-3": {
+						"url":"http://www.labor.ny.gov/youth",
+						"text":"Youth Portal"
+					}
+				}
+			}
+		},
+	"other":
+		{
+		"drop":"right",
+		"size":5,
+		"content": {
+			"container_size-1":"col_2",
+			"container_contents-1": {
+				"linkheader-1": {
+					"url": "http://www.labor.ny.gov/agencyinfo/aicontactus.shtm",
+					"text":"Contact Us"
+				},
+				"header-1":"Boards",
+				"link-1": {
+					"url":"http://www.labor.ny.gov/erb/index.asp",
+					"text":"Employment Relations"
+				},
+				"link-2": {
+					"url":"http://www.labor.ny.gov/hab/index.htm",
+					"text":"Hazard Abatement"
+				},
+				"link-3": {
+					"url":"http://www.labor.ny.gov/iba/index.asp",
+					"text":"Industrial Board of Appeals "
+				},
+				"link-4": {
+					"url":"http://www.labor.ny.gov/uiappeal/index.asp",
+					"text":"Unemployment Insrance Appeal Board"
+				},
+				"linkheader-2": {
+					"url": "http://www.labor.ny.gov/sites/legal/laws/new-laws.page",
+					"text":"Legal & Regulations"
+				},
+				"link-5": {
+					"url":"http://www.labor.ny.gov/sites/legal/laws/child-performer.page",
+					"text":"Child Performer"
+				},
+				"link-6": {
+					"url":"http://www.labor.ny.gov/sites/legal/laws/hospitality-industry-wage-order.page",
+					"text":"Hospitality Wage Order"
+				},
+				"link-7": {
+					"url":"http://www.labor.ny.gov/sites/legal/laws/construction-industry-fair-play-act.page",
+					"text":"Construction Industry Fair Play Act"
+				},
+				"link-8": {
+					"url":"http://www.labor.ny.gov/sites/legal/laws/domestic-workers-bill-of-rights.page",
+					"text":"Domestic Workers' Bill of Rights"
+				}
+			},
+			"container_size-2":"col_2",
+			"container_contents-2": {
+				"linkheader-1": {
+					"url":"http://www.labor.ny.gov/faq.shtm",
+					"text":"Labor Statistics"
+				},
+				"link-1": {
+					"url":"http://www.labor.ny.gov/stats/pressreleases/pruistat.shtm",
+					"text":"Monthly Statistics Press Release"
+				},
+				"link-2": {
+					"url":"http://www.labor.ny.gov/stats/icei.shtm",
+					"text":"Economic Index"
+				},
+				"link-3": {
+					"url":"http://www.labor.ny.gov/stats/regmap.shtm",
+					"text":"Regional Labor Market Information "
+				},
+				"linkheader-2": {
+					"url":"http://www.labor.ny.gov/newsroom.shtm",
+					"text":"Newsroom"
+				},
+				"link-4": {
+					"url":"http://www.labor.ny.gov/pressreleases/pressreleases.shtm",
+					"text":"Press Release and Publications"
+				},
+				"link-5": {
+					"url":"http://www.labor.ny.gov/agencyinfo/annualrpt.shtm",
+					"text":"Annual Reports"
+				},
+				"link-6": {
+					"url":"http://www.labor.ny.gov/stats/enys_index.shtm",
+					"text":"News Letters"
+				},
+				"link-7": {
+					"url":"http://www.labor.ny.gov/agencyinfo/publicmeeting.shtm",
+					"text":"Public Meetings"
+				},
+				"linkheader-3": {
+					"url":"http://www.labor.ny.gov/workforcenypartners/warn/warnportal.shtm",
+					"text":"WARN Notices"
+				},
+				"linkheader-4": {
+					"url":"http://www.labor.ny.gov/agencyinfo/uifraud.shtm",
+					"text":"Report UI Fraud"
+				}
+			},
+			"container_size-3":"col_1",
+			"container_contents-3": {
+				"linkheader-1": {
+					"url":"http://www.labor.ny.gov/faq.shtm",
+					"text":"FAQs"
+				},
+				"link-1": {
+					"url":"http://www.labor.ny.gov/apprenticeship/faq.shtm",
+					"text":"Apprenticeship"
+				},
+				"link-2": {
+					"url":"http://www.labor.ny.gov/businessservices/faq.shtm",
+					"text":"Business Services"
+				},
+				"link-3": {
+					"url":"http://www.labor.ny.gov/careerservices/faq.shtm",
+					"text":"Career Services"
+				},
+				"link-4": {
+					"url":"http://www.labor.ny.gov/vets/faq.shtm",
+					"text":"Veterans Services"
+				},
+				"link-5": {
+					"url":"http://www.labor.ny.gov/workerprotection/wp_faq_front.shtm",
+					"text":"Worker Protection"
+				},
+				"link-6": {
+					"url":"http://www.labor.ny.gov/stats/lsfaq.shtm",
+					"text":"Labor Statistics"
+				},
+				"linkheader-2": {
+					"url":"https://www.labor.ny.gov/secure/foilrequest.shtm",
+					"text":"FOIL Request"
+				},
+				"linkheader-3": {
+					"url":"http://www.labor.ny.gov/workforcenypartners/funding.shtm",
+					"text":"Funding Opportunites"
+				}
+			}
+		}
+	}
+}
